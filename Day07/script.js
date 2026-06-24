80f8d05aa323daffd43d3990bfd575b6fa2f56f8
Dilshad52 <dilshadmohammed547@gmail.com>
2026-06-24 14:58:27 +0530

    Day 7 completed - JavaScript Basics

---
+++ Day07/script.js
@@ -0,0 +1,31 @@
+// Variables
+
+let name = "Mohammed Dilshad";
+
+let age = 22;
+
+let course = "Python Full Stack";
+
+// Output
+
+console.log(name);
+console.log(age);
+console.log(course);
+
+// Function
+
+function showMessage() {
+
+    alert("Welcome to JavaScript!");
+
+}
+let city = "Hyderabad";
+console.log(city);
+
+let a = 10;
+let b = 20;
+
+console.log(a + b);
+
+let marks = 95;
+console.log(marks);
