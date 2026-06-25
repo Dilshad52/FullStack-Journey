9de75f7cee123e0cb1771b6904fb1e6b8ce70599
Dilshad52 <dilshadmohammed547@gmail.com>
2026-06-25 15:36:05 +0530

    Day 8 completed - JavaScript Conditions

---
+++ Day08/script.js
@@ -0,0 +1,46 @@
+function checkAge() {
+
+    let age = prompt("Enter your age:");
+
+    if (age >= 18) {
+
+        alert("You are eligible to vote.");
+
+    } else {
+
+        alert("You are not eligible to vote.");
+
+    }
+
+}
+function checkNumber() {
+
+    let number = prompt("Enter a number:");
+
+    if (number > 0) {
+
+        alert("Positive Number");
+
+    } else {
+
+        alert("Negative Number");
+
+    }
+
+}
+
+function checkMarks() {
+
+    let marks = prompt("Enter Marks:");
+
+    if (marks >= 35) {
+
+        alert("Pass");
+
+    } else {
+
+        alert("Fail");
+
+    }
+
+}
